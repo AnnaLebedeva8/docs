c72b0136acf1028d9e3e348cf68b654a9fd86d6a
Hanna Lebedieva <ganna.lebedeva8@gmail.com>
2022-12-23 12:41:37 +0200

    gitignore fix

---
+++ public_html/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('C:\\Users\\hanna.lebedieva\\my-website\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('C:\\Users\\hanna.lebedieva\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('C:\\Users\\hanna.lebedieva\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('C:\\Users\\hanna.lebedieva\\my-website\\src\\css\\custom.css'),
+];
